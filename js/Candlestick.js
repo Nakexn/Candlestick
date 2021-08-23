5180c97414826307f4d9da31b533aa03e9bffc4f
那可心 <nakx@raysdata.com>
2021-08-24 01:22:56 +0800

    处理y轴数据

--- js/Candlestick.js
+++ js/Candlestick.js
@@ -25,17 +25,22 @@
     this.option = {
       style: {
         padding: 24,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        fontFamily: 'sans-serif',
+        fontWeight: 400,
+        fontSize: 12,
+        lineHeight: 1.4
       },
       xAxis: {
         paddingTop: 8,
         color: '#999',
-        borderColor: '#333'
+        borderColor: '#bbb'
       },
       yAxis: {
-        paddingRight: 8,
+        paddingRight: 12,
         color: '#999',
-        borderColor: '#ddd'
+        borderColor: '#ddd',
+        interval: 5
       },
       color: {
         increase: '#eb5454',
@@ -66,36 +71,19 @@
     this.$ctx.clearRect(0, 0, this.$canvas.width, this.$canvas.height);
     this.$ctx.fillStyle = this.option.style.backgroundColor;
     this.$ctx.fillRect(0, 0, this.$canvas.width, this.$canvas.height);
+    this.font = `${this.option.style.fontWeight} ${this.option.style.fontSize}px ${this.option.style.fontFamily}`;
 
-    // 调整坐标系原点到左下角
-    this.$ctx.save();
-    this.$ctx.translate(0, this.$el.clientHeight);
-    this.$ctx.scale(1, -1);
-
-    this.drawXAxis();
     this.drawYAxis();
+    this.drawXAxis();
     this.drawCandle();
-
-    this.$ctx.restore();
   };
 
-  Candlestick.prototype.drawXAxis = function () {
-    this.len = this.option.data.length;
-    this.colWidth = this.$el.clientWidth / this.len;
-
-    // x轴
-    this.$ctx.strokeStyle = this.option.xAxis.borderColor;
-    this.$ctx.beginPath();
-    this.$ctx.moveTo(0, 0);
-    this.$ctx.lineTo(this.$el.clientWidth, 0);
-    this.$ctx.stroke();
-    this.$ctx.closePath();
-  };
-
+  // y轴
   Candlestick.prototype.drawYAxis = function () {
     let maxValue, minValue;
     let maxArray = [],
-      minArray = [];
+      minArray = [],
+      labelArray = [];
 
     this.option.data.forEach(item => {
       maxArray.push(item[3]);
@@ -104,27 +92,102 @@
 
     maxValue = findMax(...maxArray);
     minValue = findMin(...minArray);
+
+    this.maxValue = maxValue;
+    this.minValue = minValue;
+
     this.relativeHeight = maxValue - minValue;
 
+    for (let i = 0; i < this.option.yAxis.interval; i++) {
+      let intervalValue = (
+        minValue +
+        (i * (maxValue - minValue)) / this.option.yAxis.interval
+      ).toFixed(2);
+      labelArray.push(intervalValue);
+    }
+    labelArray.push(maxValue.toFixed(2));
+
+    let labelWidth = this.$ctx.measureText(`${maxValue.toFixed(2)}`).width;
+    let labelHeight = this.option.style.fontSize * this.option.style.lineHeight;
+
+    this.labelWidth = labelWidth;
+    this.labelHeight = labelHeight;
+
     this.$ctx.strokeStyle = this.option.yAxis.borderColor;
-    this.rowHeight = this.$el.clientHeight / 5;
-    //y轴分割线
-    for (let i = 1; i <= 5; i++) {
+    this.rowHeight =
+      (this.$el.clientHeight -
+        labelHeight -
+        this.option.xAxis.paddingTop -
+        this.option.style.padding * 2) /
+      this.option.yAxis.interval;
+
+    this.seriesLeft = this.option.style.padding + labelWidth + this.option.yAxis.paddingRight;
+    this.seriesBottom = this.option.style.padding + labelHeight + this.option.xAxis.paddingTop;
+
+    //y轴标签和分割线
+    for (let i = 1; i <= this.option.yAxis.interval; i++) {
+      // 调整坐标系原点到左下角;
+      this.$ctx.save();
+      this.$ctx.translate(0, this.$el.clientHeight);
+      this.$ctx.scale(1, -1);
+
       this.$ctx.beginPath();
-      this.$ctx.moveTo(0, i * this.rowHeight);
-      this.$ctx.lineTo(this.$el.clientWidth, i * this.rowHeight);
+      this.$ctx.moveTo(this.seriesLeft, i * this.rowHeight + this.seriesBottom);
+      this.$ctx.lineTo(
+        this.$el.clientWidth - this.option.style.padding,
+        i * this.rowHeight + this.seriesBottom
+      );
       this.$ctx.stroke();
       this.$ctx.closePath();
+
+      this.$ctx.restore();
+
+      this.$ctx.font = this.font;
+      this.$ctx.textAlign = 'right';
+      this.$ctx.fillStyle = this.option.yAxis.color;
+      this.$ctx.fillText(
+        `${labelArray[i]}`,
+        this.seriesLeft - this.option.yAxis.paddingRight,
+        this.$el.clientHeight - this.seriesBottom - i * this.rowHeight + labelHeight / 4
+      );
     }
+    this.$ctx.fillText(
+      `${labelArray[0]}`,
+      this.seriesLeft - this.option.yAxis.paddingRight,
+      this.$el.clientHeight - this.seriesBottom + labelHeight / 4
+    );
+  };
+
+  // x轴
+  Candlestick.prototype.drawXAxis = function () {
+    this.$ctx.strokeStyle = this.option.xAxis.borderColor;
+
+    // 调整坐标系原点到左下角;
+    this.$ctx.save();
+    this.$ctx.translate(0, this.$el.clientHeight);
+    this.$ctx.scale(1, -1);
+
+    this.$ctx.beginPath();
+    this.$ctx.moveTo(this.seriesLeft, this.seriesBottom);
+    this.$ctx.lineTo(this.$el.clientWidth - this.option.style.padding, this.seriesBottom);
+    this.$ctx.stroke();
+    this.$ctx.closePath();
+
+    this.$ctx.restore();
   };
 
   Candlestick.prototype.drawCandle = function () {
+    this.len = this.option.data.length;
+    this.colWidth = (this.$el.clientWidth - this.seriesLeft - this.option.style.padding) / this.len;
     this.option.data.forEach(drawRect.bind(this));
 
     function drawRect(item, index) {
       let x = this.colWidth * index + this.colWidth * 0.25;
       let xLine = this.colWidth * index + this.colWidth * 0.5;
-      let y = (findMin(item[0], item[1]) / this.relativeHeight) * this.$el.clientHeight;
+      let y =
+        ((findMin(item[0], item[1]) - this.minValue) / this.relativeHeight) *
+          (this.$el.clientHeight - this.seriesBottom - this.option.style.padding) +
+        this.seriesBottom;
       let width = this.colWidth * 0.5;
       let dValue = item[1] - item[0];
       if (dValue >= 0) {
@@ -134,14 +197,33 @@
         this.$ctx.fillStyle = this.option.color.decrease;
         this.$ctx.strokeStyle = this.option.color.decrease;
       }
-      let height = (calcAbs(dValue) / this.relativeHeight) * this.$el.clientHeight;
-      this.$ctx.fillRect(x, y, width, height);
+      let height =
+        (calcAbs(dValue) / this.relativeHeight) *
+        (this.$el.clientHeight - this.seriesBottom - this.option.style.padding);
 
+      // 调整坐标系原点;
+      this.$ctx.save();
+      this.$ctx.translate(this.seriesLeft, this.$el.clientHeight);
+      this.$ctx.scale(1, -1);
+
+      this.$ctx.fillRect(x, y, width, height);
       this.$ctx.beginPath();
-      this.$ctx.moveTo(xLine, (item[3] / this.relativeHeight) * this.$el.clientHeight);
-      this.$ctx.lineTo(xLine, (item[2] / this.relativeHeight) * this.$el.clientHeight);
+      this.$ctx.moveTo(
+        xLine,
+        ((item[3] - this.minValue) / this.relativeHeight) *
+          (this.$el.clientHeight - this.seriesBottom - this.option.style.padding) +
+          this.seriesBottom
+      );
+      this.$ctx.lineTo(
+        xLine,
+        ((item[2] - this.minValue) / this.relativeHeight) *
+          (this.$el.clientHeight - this.seriesBottom - this.option.style.padding) +
+          this.seriesBottom
+      );
       this.$ctx.stroke();
       this.$ctx.closePath();
+
+      this.$ctx.restore();
     }
   };
 
@@ -159,7 +241,8 @@
     canvas.height = h * ratio;
     canvas.style.width = w + 'px';
     canvas.style.height = h + 'px';
-    canvas.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0);
+    let ctx = canvas.getContext('2d');
+    ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
     return canvas;
   }
 
